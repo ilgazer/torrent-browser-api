585e236b8c8381343b8ccbb1f4d417fc1a575e99
ilgazer <er.ilgaz@gmail.com>
2019-08-09 22:01:28 +0300

    Added more image support

--- routes/add_torrent.js
+++ routes/add_torrent.js
@@ -7,7 +7,7 @@ module.exports = ({router}, {delugeApi}, {dao}, image_dir) => {
                 .then((json) => dao.saveTorrent(ctx.request.body.title, ctx.request.body.author,
                     ctx.request.files[1].path.split("/").pop(), json.result.files_tree.contents))
                 .then(() => console.log("Add torrent to Mongo successful"),
-                    reason => console.log("Rejected promise on getTorrentInfo" + reason));
+                    reason => console.log("Rejected promise on getTorrentInfo" + reason,ctx.request.files));
             delugeApi.addTorrentFromFile(ctx.request.files[0].path)
                 .then(() => console.log("Add torrent to Deluge successful"),
                     reason => console.log("Rejected promise on addTorrentFromFile" + reason));
