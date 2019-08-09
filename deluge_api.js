9e76759fa5c30238c7b867c754ce3370c0fd4ccc
ilgazer <er.ilgaz@gmail.com>
2019-08-09 15:54:30 +0300

    Migrated to Promise

--- deluge_api.js
+++ deluge_api.js
@@ -1,35 +1,40 @@
-module.exports = function (url,password) {
+module.exports = function (url, password) {
     const request = require('request');
-    const delugeRequest = (method, params, callback) => {
-        request.post({
-            url: url,
-            headers: {
-                'content-type': 'application/json',
-            },
-            gzip: true,
-            jar: true,
-            body: JSON.stringify({
-                "method": method,
-                id: 100,
-                params: params
-            })
-        }, (err, httpResponse, body) =>
-            callback(JSON.parse(body,
-                function (k, v) {
-                    return (typeof v === "object" || typeof v === "boolean" || isNaN(v)) ? v : parseInt(v, 10);
+    const delugeRequest = (method, params) => {
+        return new Promise(function (resolve, reject) {
+            request.post({
+                url: url,
+                headers: {
+                    'content-type': 'application/json',
+                },
+                gzip: true,
+                jar: true,
+                body: JSON.stringify({
+                    "method": method,
+                    id: 100,
+                    params: params
+                })
+            }, (err, httpResponse, body) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(JSON.parse(body,
+                        function (k, v) {
+                            return (typeof v === "object" || typeof v === "boolean" || isNaN(v)) ? v : parseInt(v, 10);
+                        }
+                    ));
                 }
-            )))
-
+            });
+        });
     };
     delugeRequest("auth.login",
-        [password],
-        (json) => {
-            if (json.result)
-                console.log("Login successful");
-            else
-                console.log("Login failed");
+        [password]).then((json) => {
+        if (json.result)
+            console.log("Login successful");
+        else
+            console.log("Login failed");
 
-        });
+    });
     const addTorrentFromFile = (filename) => {
         //console.log(filename);
         delugeRequest("core.get_config_values",
@@ -43,35 +48,34 @@ module.exports = function (url,password) {
                 "max_upload_slots_per_torrent",
                 "max_upload_speed_per_torrent",
                 "prioritize_first_last_pieces"
-            ]],
-            (json) => {
-                delugeRequest("web.add_torrents",
-                    [[{
-                        "path": filename,
-                        "options": {
-                            "file_priorities": [
-                                1
-                            ],
-                            "add_paused": false,
-                            "max_download_speed": json.result.max_download_speed_per_torrent,
-                            "prioritize_first_last_pieces": json.result.prioritize_first_last_pieces,
-                            "max_upload_speed": json.result.max_upload_speed_per_torrent,
-                            "max_connections": json.result.max_connections_per_torrent,
-                            "move_completed_path": json.result.move_completed_path,
-                            "download_location": json.result.download_location,
-                            "compact_allocation": json.result.compact_allocation,
-                            "move_completed": json.result.move_completed,
-                            "max_upload_slots": json.result.max_upload_slots_per_torrent
-                        }
-                    }]],
-                    (json) => {
-                        console.log(json.result);
+            ]])
+            .then((json) => delugeRequest("web.add_torrents",
+                [[{
+                    "path": filename,
+                    "options": {
+                        "file_priorities": [
+                            1
+                        ],
+                        "add_paused": false,
+                        "max_download_speed": json.result.max_download_speed_per_torrent,
+                        "prioritize_first_last_pieces": json.result.prioritize_first_last_pieces,
+                        "max_upload_speed": json.result.max_upload_speed_per_torrent,
+                        "max_connections": json.result.max_connections_per_torrent,
+                        "move_completed_path": json.result.move_completed_path,
+                        "download_location": json.result.download_location,
+                        "compact_allocation": json.result.compact_allocation,
+                        "move_completed": json.result.move_completed,
+                        "max_upload_slots": json.result.max_upload_slots_per_torrent
                     }
-                )
-            })
+                }]]))
+            .then(
+                (json) => {
+                    console.log(json.result);
+                }
+            )
     };
-    const getTorrentInfo = (filename, callback) => {
-        delugeRequest("web.get_torrent_info", [filename], callback);
+    const getTorrentInfo = (filename) => {
+        return delugeRequest("web.get_torrent_info", [filename]);
     };
     return {
         addTorrentFromFile: addTorrentFromFile,
--- routes/add_torrent.js
+++ routes/add_torrent.js
@@ -1,15 +1,16 @@
 const busboy = require('koa-busboy')();
+const moveFile = require('move-file');
 
 module.exports = ({router}, {delugeApi}, {dao}) => {
     router.post("/add_torrent", busboy, (ctx) => {
-        delugeApi.getTorrentInfo(ctx.request.files[0].path,
+        delugeApi.getTorrentInfo(ctx.request.files[0].path).then(
             (json) => {
+                console.log(ctx.request.files[0].path);
                 if (!json.result || json.error !== null) {
                     throw new Error("get_torrent_info failed:\n" + JSON.stringify(json));
                 }
                 dao.saveTorrent(ctx.request.body.title, ctx.request.body.author, "", json.result.files_tree.contents);
             });
         delugeApi.addTorrentFromFile(ctx.request.files[0].path);
-        ctx.body = ctx.request.body.toString();
     })
 };
