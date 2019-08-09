37af28fb5c8f22c30a4bffc935e20fb1c7724f2d
ilgazer <er.ilgaz@gmail.com>
2019-08-10 00:39:29 +0300

    Changed CORS settings

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ async function init() {
     const Koa = require('koa');
     const Router = require('koa-router');
     const DelugeApi = require('./deluge_api');
+    const cors = require('koa2-cors');
+
     const MongoDao = require('./MongoDao');
-
     let config = require('minimist')(process.argv.slice(2));
-    console.log(config);
 
+    console.log(config);
     const koa = new Koa();
     const router = new Router();
     const delugeApi = DelugeApi(config.deluge_url, config.deluge_pass);
-    const dao = await MongoDao(config.mongo_url);
 
+    const dao = await MongoDao(config.mongo_url);
     require("./routes/basic")({router});
     require("./routes/add_torrent")({router}, {delugeApi}, {dao}, config.image_dir);
     require("./routes/list_torrents")({router}, {dao});
+
+    koa.use(cors({
+        origin: "*",
+        allowMethods: ['GET', 'POST', 'DELETE'],
+    }));
 
     koa.use(router.routes());
     koa.use(router.allowedMethods());
