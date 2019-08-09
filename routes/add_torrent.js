d43b5d53f213e4dd14a6c4ffa024045f110f2746
ilgazer <er.ilgaz@gmail.com>
2019-08-09 21:44:10 +0300

    Added more image support

--- routes/add_torrent.js
+++ routes/add_torrent.js
@@ -1,15 +1,14 @@
-const busboy = require('koa-busboy')();
-const moveFile = require('move-file');
-
 module.exports = ({router}, {delugeApi}, {dao}, image_dir) => {
-    router.post("/add_torrent", busboy, (ctx) => {
-        let filename=Date.now() + ctx.request.files[1].filename;
-        Promise.all([delugeApi.getTorrentInfo(ctx.request.files[0].path),
-            moveFile(ctx.request.files[1].path, image_dir + "/" + filename)]).then(
-            ([json]) => {
-                dao.saveTorrent(ctx.request.body.title, ctx.request.body.author, filename, json.result.files_tree.contents);
-            }, reason => console.log("Rejected promise on getTorrentInfo" + reason));
-        delugeApi.addTorrentFromFile(ctx.request.files[0].path).then(() => console.log("Add torrent successful"),
-            reason => console.log("Rejected promise on addTorrentFromFile" + reason));
-    })
+    router.post("/add_torrent",
+        require('koa-busboy')({
+            dest: image_dir
+        }), (ctx) => {
+            delugeApi.getTorrentInfo(ctx.request.files[0].path)
+                .then((json) => {
+                    dao.saveTorrent(ctx.request.body.title, ctx.request.body.author,
+                        ctx.request.files[1].path.split("/").pop(), json.result.files_tree.contents);
+                }, reason => console.log("Rejected promise on getTorrentInfo" + reason));
+            delugeApi.addTorrentFromFile(ctx.request.files[0].path).then(() => console.log("Add torrent successful"),
+                reason => console.log("Rejected promise on addTorrentFromFile" + reason));
+        })
 };
