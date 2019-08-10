d2cb65002c0b0a76b893483e03cbf8d6599db1d3
ilgazer <er.ilgaz@gmail.com>
2019-08-10 12:49:13 +0300

    Fixed bug where folders were not being parsed properly

--- routes/add_torrent.js
+++ routes/add_torrent.js
@@ -4,8 +4,17 @@ module.exports = ({router}, {delugeApi}, {dao}, image_dir) => {
             dest: image_dir
         }), (ctx) => {
             delugeApi.getTorrentInfo(ctx.request.files[0].path)
-                .then((json) => dao.saveTorrent(ctx.request.body.title, ctx.request.body.author,
-                    ctx.request.files[1].path.split("/").pop(), json.result.files_tree.contents))
+                .then((json) => {
+                    function contents() {
+                        if (json.result.files_tree.type === "dir") {
+                            return {[json.result.name]: json.result.files_tree};
+                        } else {
+                            return json.result.files_tree.contents
+                        }
+                    }
+                    return dao.saveTorrent(ctx.request.body.title, ctx.request.body.author,
+                        ctx.request.files[1].path.split("/").pop(), contents())
+                })
                 .then(() => console.log("Add torrent to Mongo successful"),
                     reason => console.log("Rejected promise on getTorrentInfo" + reason, ctx.request.files));
             delugeApi.addTorrentFromFile(ctx.request.files[0].path)
